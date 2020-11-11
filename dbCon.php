b25ee9ad9aa0af0fe5eee5b97024b82595740320
philipp-klug <philipp-klug@web.de>
2020-11-11 15:05:49 +0100

    [Snap] Added app folder with add.php for INSERT statement

--- dbCon.php
+++ dbCon.php
@@ -9,7 +9,6 @@ $dbName = "to_do_list";
 try {
     $conn = new PDO("mysql:host=$serverName;dbname=$dbName", $userName, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connection";
 }catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
--- index.php
+++ index.php
@@ -11,32 +11,50 @@
 </head>
 <body>
     <div class="main-section">
+
+        <!-- Section to add a To-Do -->
         <div class="add-section">
-            <form action="" method="POST" autocomplete="off">
-                <input type="text" name="input text" placeholder="required*" />
-                <button type="submit">Add &nbsp; <span>&#43</span></button>
+            <form action="app/add.php" method="POST" autocomplete="off">
+
+                <!-- error handling, if message=error isset-->
+                <?php if(isset($_GET['message']) && $_GET['message'] == 'error') { ?>
+                    <input type="text" name="title" style="border-color: #ff6666" placeholder="No To-Do entered! " />
+                    <button type="submit">Add &nbsp; <span>&#43</span></button>
+                <?php } else { ?>
+                    <input type="text" name="title" placeholder="required *" />
+                    <button type="submit">Add &nbsp; <span>&#43</span></button>
+                <?php } ?>
             </form>
         </div>
+
+        <!-- DB query to get all data -->
         <?php
             $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
         ?>
+
+        <!-- Section to Show To-Dos -->
         <div class="show-todo-section">
+            <!-- Shown, if To-Do List is empty -->
             <?php if($todos->rowCount() === 0) { ?>
                 <div class="todo-item">
                    NO TO DOS TO DO <!--<img src="img/blank.jpg" width="100%" />-->
                 </div>
             <?php } ?>
 
+            <!-- While loop to print all To-Do -->
             <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
                 <div class="todo-item">
+                    <!-- If To-Do is checked -->
                     <span id="<?php echo $todo['id']; ?>" class="remove-to-do">x</span>
                     <?php if($todo['checked']) { ?>
                         <input type="checkbox" class="check-box" checked />
                         <h2 class="checked"><?php echo $todo['title'] ?></h2>
+                    <!-- Otherwise To-Do is unchecked -->
                     <?php } else { ?>
                         <input type="checkbox" class="check-box" />
                         <h2><?php echo $todo['title'] ?></h2>
                     <?php } ?>
+                    <!-- Allways shown created Datetime -->
                     <br />
                     <small>created: <?php echo $todo['date_time'] ?></small>
                 </div>
