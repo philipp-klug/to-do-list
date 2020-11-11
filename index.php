1b2533bf2c890544554a96ced9079b2a8184a43f
philipp-klug <philipp-klug@web.de>
2020-11-11 22:16:01 +0100

    [Snap] Added remove and check. Started with js and css

--- index.php
+++ index.php
@@ -20,9 +20,9 @@
 
         <!-- error handling, if message=error isset-->
         <?php if(isset($_GET['message']) && $_GET['message'] == 'error') { ?>
-            <input type="text" class="form-control" placeholder="Enter To-Do" style="border-color: #ff6666" > <!-- Leave off 'required autofocus' to show error handling -->
+            <input type="text" name="title" class="form-control" placeholder="Enter To-Do" style="border-color: #ff6666" > <!-- Leave off 'required autofocus' to show error handling -->
         <?php } else { ?>
-            <input type="text" class="form-control" placeholder="Enter To-Do"> <!-- Leave off 'required autofocus' to show error handling  -->
+            <input type="text" name="title" class="form-control" placeholder="Enter To-Do"> <!-- Leave off 'required autofocus' to show error handling  -->
         <?php } ?>
 
         <button class="btn btn-lg btn-primary btn-block" type="submit">Do it!</button>
@@ -46,18 +46,23 @@
                     <div class="todo-item">
                         <!-- If To-Do is checked -->
                         <?php if($todo['checked']) { ?>
-                            <li class="list-group-item disabled" aria-disabled="true"><?php echo $todo['title'] ?>
-                                <small>created: <?php echo $todo['date_time'] ?></small>
+                            <li class="list-group-item" aria-disabled="true">
+                                <p><s><?php echo $todo['title'] ?></s></p>
                                 <br/>
-                                <input type="checkbox" class="check-box" checked />
-                                <span id="<?php echo $todo['id']; ?>" class="remove-to-do">x</span>
+                                <small>created: <?php echo $todo['date_time'] ?></small>
+                                <br />
+                                <input type="checkbox" class="check-box" data-todo-id="<?php echo $todo['id']; ?>" checked />
+                                <span id="<?php echo $todo['id']; ?>" class="remove-to-do" aria-disabled="false">x</span>
                             </li>
                             <!-- Otherwise To-Do is unchecked -->
                         <?php } else { ?>
-                            <li class="list-group-item"><?php echo $todo['title'] ?>
+                            <li class="list-group-item">
+                                <p><?php echo $todo['title'] ?></p>
                                 <br />
                                 <small>created: <?php echo $todo['date_time'] ?></small>
-                                <input type="checkbox" class="check-box" />
+                                <br />
+                                <input type="checkbox" class="check-box" data-todo-id="<?php echo $todo['id']; ?>" />
+                                <span id="<?php echo $todo['id']; ?>" class="remove-to-do">x</span>
                             </li>
                         <?php } ?>
                     </div>
@@ -68,5 +73,47 @@
     <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+     <!-- jquery Js-->
+    <script src="js/jquery-3.2.1.min.js"></script>
+
+    <!-- Own Js-->
+    <script>
+        $(document).ready(function () {
+            //remove animation
+            $('.remove-to-do').click(function(){
+                const id = $(this).attr('id');
+               $.post("app/remove.php", {
+                       id: id
+                   },
+                   (data) => {
+                        if(data) {
+                            $(this).parent().hide(600);
+                        }
+                   }
+                );
+            });
+
+            //checked toggle
+            $(".check-box").click(function(e) {
+                const id = $(this).attr('data-todo-id');
+                $.post('app/check.php',
+                    {
+                        id: id
+                    },
+                    (data) => {
+                        if(data != 'error') {
+                            const p = $(this).next();
+                            if(data === '1') {
+                                p.removeClass('checked');
+                            } else {
+                                p.addClass('checked');
+                            }
+                        }
+                    }
+                )
+            });
+        });
+    </script>
 </body>
 </html>
